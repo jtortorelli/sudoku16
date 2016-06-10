cd60ed02ecddc213d2c31db11ecf8ec77fd80d0f
James Tortorelli <James.Tortorelli@lifeway.com>
2016-06-10 15:06:04 -0500

    Tweak error messaging

--- sudoku16_solver.py
+++ sudoku16_solver.py
@@ -16,17 +16,17 @@ def main(argv):
     lines = [line.rstrip() for line in src]
     src.close()
     if len(lines) != 16:
-        sys.exit('File does not contain valid puzzle (expected 16 lines, read ' + str(len(lines)) + ' lines)')
+        sys.exit('File does not contain valid puzzle; expected 16 lines, found ' + str(len(lines)) + ' lines')
     for i, line in enumerate(lines):
         if len(line) != 16:
-            sys.exit('File does not contain valid puzzle (expected 16 columns, found ' + str(len(line)) + ' columns on line ' + str(i + 1) + ')')
+            sys.exit('File does not contain valid puzzle; expected 16 columns, found ' + str(len(line)) + ' columns on line ' + str(i + 1) + '')
     master_set = set()
     for line in lines:
         for n in line:
             if n != '-':
                 master_set.add(n)
     if len(master_set) != 16:
-        sys.exit("File does not contain valid puzzle (expected 16 unique symbols, found " + str(len(master_set)) + ")")
+        sys.exit("File does not contain valid puzzle; expected 16 unique symbols, found " + str(len(master_set)) + ": " + ', '.join(sorted(master_set)))
     # import puzzle as matrix
     # print the puzzle start state
     # create master set
