2c58402129682be16ce903506305410184ed4e91
James Tortorelli <James.Tortorelli@lifeway.com>
2016-06-10 14:23:34 -0500

    Add validations for puzzle rows and columns

--- sudoku16_solver.py
+++ sudoku16_solver.py
@@ -11,6 +11,14 @@ def main(argv):
         sys.exit('Must supply plain text file (using .txt extension)')
     if not os.path.isfile(argv[1]):
         sys.exit('File not found: ' + argv[1])
+    filename = argv[1]
+    src = open(filename, 'r')
+    lines = [line.rstrip() for line in src]
+    if len(lines) != 16:
+        sys.exit('File does not contain valid puzzle (expected 16 lines, read ' + str(len(lines)) + ' lines)')
+    for i, line in enumerate(lines):
+        if len(line) != 16:
+            sys.exit('File does not contain valid puzzle (expected 16 columns, found ' + str(len(line)) + ' columns on line ' + str(i + 1) + ')')
     # validate file contains valid puzzle
     # import puzzle as matrix
     # print the puzzle start state
