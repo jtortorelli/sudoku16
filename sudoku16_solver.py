abac5eb3c2d6a07ffcf06c843da502b8331c5280
James Tortorelli <James.Tortorelli@lifeway.com>
2016-06-10 13:02:16 -0500

    create main script

---
+++ sudoku16_solver.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+
+def main():
+    # receive filename argument from user
+    # validate file exists
+    # validate file contains valid puzzle
+    # import puzzle as matrix
+    # print the puzzle start state
+    # create master set
+    # create sets for rows, cols, and squares
+    # create list of empty spaces
+    # start tracking time
+    # start solving
+    pass
