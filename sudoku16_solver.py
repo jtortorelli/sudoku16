df93946965304cf93b0b9a9867a0191c73f20d14
James Tortorelli <James.Tortorelli@lifeway.com>
2016-06-10 13:08:43 -0500

    add file validations

--- sudoku16_solver.py
+++ sudoku16_solver.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python
 
+import sys
+import os.path
 
-def main():
-    # receive filename argument from user
-    # validate file exists
+
+def main(argv):
+    if len(argv) < 1 or len(argv) > 1:
+        print('Requires exactly one argument (filename)')
+        return
+    if not os.path.isfile(argv[0]):
+        print('File not found: ' + argv[0])
+        return
     # validate file contains valid puzzle
     # import puzzle as matrix
     # print the puzzle start state
@@ -13,3 +20,7 @@ def main():
     # start tracking time
     # start solving
     pass
+
+
+if __name__ == "__main__":
+    main(sys.argv)
