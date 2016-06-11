ad9aaca60956cc3bd13f54ae8c9bbd7a9d4b8a42
James Tortorelli <James.Tortorelli@lifeway.com>
2016-06-11 17:14:52 -0500

    add comments, create row sets

--- sudoku16_solver.py
+++ sudoku16_solver.py
@@ -5,16 +5,25 @@ import os.path
 
 
 def main(argv):
+    # check correct number of arguments
     if len(argv) < 2 or len(argv) > 2:
         sys.exit('Requires exactly one argument (filename)')
+
+    # check that argument ends in '.txt'
     if not argv[1].endswith('.txt'):
         sys.exit('Must supply plain text file (using .txt extension)')
+
+    # check that argument is reference to valid file
     if not os.path.isfile(argv[1]):
         sys.exit('File not found: ' + argv[1])
+
+    # read in puzzle
     filename = argv[1]
     src = open(filename, 'r')
     lines = [line.rstrip() for line in src]
     src.close()
+
+    # validate puzzle format
     if len(lines) != 16:
         sys.exit('File does not contain valid puzzle; expected 16 lines, found ' + str(len(lines)) + ' lines')
     for i, line in enumerate(lines):
@@ -27,12 +36,26 @@ def main(argv):
                 master_set.add(n)
     if len(master_set) != 16:
         sys.exit("File does not contain valid puzzle; expected 16 unique symbols, found " + str(len(master_set)) + ": " + ', '.join(sorted(master_set)))
+
+    # create matrix structure to hold puzzle information
     matrix = []
     for line in lines:
         row = []
         for n in line:
             row.append(n)
         matrix.append(row)
+
+    # create row sets
+    rows = []
+    for i, n in enumerate(matrix):
+        row = set()
+        for m in n:
+            if m != '-':
+                if m in row:
+                    sys.exit("File does not contain valid puzzle; found duplicate value " + m + " in row " + str(i))
+                else:
+                    row.add(m)
+        rows.append(row)
 
     # import puzzle as matrix
     # print the puzzle start state
